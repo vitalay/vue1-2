1d87fe1220d97eff55b5e9b2d0ffe3767321554a
vitalya.efimov <vitalay.efimov@gmail.com>
2024-11-12 20:17:48 +0500

    1

--- vitest.config.js
+++ vitest.config.js
@@ -1,21 +1,19 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+// import { fileURLToPath } from 'node:url'
+// import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+// import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      publicPath: process.env.NODE_ENV === 'production' ? '/vue1-2/' : '/',
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-  }),
-)
-
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   publicPath: process.env.NODE_ENV === 'production' ? '/testttt/' : '/',
-//   transpileDependencies: true
-// })
+// export default mergeConfig(
+//   viteConfig,
+//   defineConfig({
+//     test: {
+//       environment: 'jsdom',
+//       exclude: [...configDefaults.exclude, 'e2e/**'],
+//       root: fileURLToPath(new URL('./', import.meta.url)),
+//     },
+//   }),
+// )
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue1-2/' : '/',
+  transpileDependencies: true
+})
